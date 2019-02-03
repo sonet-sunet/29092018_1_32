2782290c9d07b9d13cf0eb5a9f2196ced4063606
sonet-sunet <slava.zhukov@gmail.com>
2019-02-03 12:13:26 +0300

    Добавлена структура

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
-<?php 
-    echo "Привет, я в GIT";
-?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Главная страница</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+    <h1>Эта наша супер страница</h1>
+
+    <script src="/js/main.js"></script>
+</body>
+</html>
+
+<!-- 
+    git status
+    git add .
+    git commit -m 'Comment'
+    git push
+ -->
